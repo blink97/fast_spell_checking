cf9e6c02fe5a0203a7623d0cfe6c2ddf5cd0fc84
Etienne Gandilhon <etienne.gandilhon@epita.fr>
2019-07-22 22:33:31 +0200

    compiler: adding trie

---
+++ src/compiler/trie.cc
@@ -0,0 +1,30 @@
+#include "trie.hh"
+
+Node::Node(std::string value) : value_(value)
+{}
+
+Node *Node::get_child(std::string value)
+{
+    auto res = (*this).children_.find(value);
+    if (res == (*this).children_.end())
+        return NULL;
+    return &(res->second);
+}
+
+void Node::insert_child(std::string value){
+    (*this).children_.insert(std::pair<std::string, Node>(value, Node(value)));
+}
+
+void insert(Node &node, std::string value)
+{
+    for (size_t i = 0; i < value.length(); i++)
+    {
+        std::string c = value.substr(i, 1);
+        if (node.get_child(c) == NULL) 
+        {
+            node.insert_child(c);
+        }
+        node = *(node.get_child(c));
+    }
+    
+}
--- src/compiler/trie.hh
+++ src/compiler/trie.hh
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <string>
+#include <map>
+
 class Node
 {
 private:
     std::string value_;
     std::map<std::string, Node> children_;
 public:
+    Node() = default;
+    Node(std::string value);
     std::string get_value() const { return value_; };
-    Node get_child(std::string);
+    Node *get_child(std::string value);
+    void insert_child(std::string value);
+};
 
-    void insert(std::string);
-}
+void insert(Node &node, std::string value);
