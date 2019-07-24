7230c11b72d7085d8456f224cf41d2ce493009b7
Etienne Gandilhon <etienne.gandilhon@epita.fr>
2019-07-24 02:11:35 +0200

    compiler: patricia trie done

--- src/compiler/trie.cc
+++ src/compiler/trie.cc
@@ -3,6 +3,11 @@
 Node::Node(std::string value) : value_(value)
 {}
 
+Node::Node(Node *node, std::string value) : value_(value)
+{
+    children_.insert(std::pair<std::string, Node*>(node->get_value(), node));
+}
+
 Node::~Node()
 {
     for(auto it = (*this).children_.begin(); it != (*this).children_.end(); ++it)
@@ -19,9 +24,27 @@ Node *Node::get_child(std::string value)
     return res->second;
 }
 
+void Node::set_child(std::string value, Node *node)
+{
+    auto it = (*this).children_.find(value);
+    it->second = node;
+}
+
 void Node::insert_child(std::string value)
 {
     (*this).children_.insert(std::pair<std::string, Node*>(value, new Node(value)));
+}
+
+void Node::insert_child(std::string value, Node *node)
+{
+    (*this).children_.insert(std::pair<std::string, Node*>(value, node));
+}
+
+void Node::set_child_key(std::string old_key, std::string new_key)
+{
+    auto nodeHandler = (*this).children_.extract(old_key);
+    nodeHandler.key() = new_key;
+    (*this).children_.insert(std::move(nodeHandler));
 }
 
 std::string commonPrefixUtil(std::string str1, std::string str2) 
@@ -75,8 +98,27 @@ void insert(Node *node, std::string value)
     if (prefix != "") // Word as a commun prefix with value
     {
         std::string real_prefix = commonPrefixUtil(prefix, value);
+        std::string suffix = value.substr(real_prefix.length(), value.length() - real_prefix.length());
+        std::string suffix_commun = prefix.substr(real_prefix.length(), prefix.length() - real_prefix.length());
         if (node->get_child(real_prefix) != NULL) { // Word is a prefix of value
-            insert(node->get_child(real_prefix), value.substr(real_prefix.length(), value.length() - real_prefix.length()));
+            insert(node->get_child(real_prefix), suffix);
+        }
+        else
+        {   
+            std::cout << "old_part" << std::endl;
+            Node *old_part = node->get_child(prefix);
+            old_part->set_value(suffix_commun);
+
+            std::cout << "new_node" << std::endl;
+            Node *new_node  = new Node(real_prefix);
+            node->set_child_key(prefix, real_prefix);
+
+            std::cout << "insert" << std::endl;
+            new_node->insert_child(suffix_commun, old_part);
+            new_node->insert_child(suffix);
+
+            std::cout << "set" << std::endl;
+            node->set_child(real_prefix, new_node);
         }
     }
     else {
--- src/compiler/trie.hh
+++ src/compiler/trie.hh
@@ -12,10 +12,19 @@ private:
 public:
     Node() = default;
     Node(std::string value);
+    Node(Node* node, std::string value);
     ~Node();
+    
     std::string get_value() const { return value_; };
+    void set_value(std::string value) { value_ = value; };
+
     Node *get_child(std::string value);
+    void set_child(std::string value, Node *node);
+
     void insert_child(std::string value);
+    void insert_child(std::string value, Node *node);
+    void set_child_key(std::string old_key, std::string new_key);
+
     std::string commun_prefix(std::string value);
 
     friend std::ostream& operator <<(std::ostream& os, const Node& node);
